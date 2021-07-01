61789a549aa59edf1d5112ac3352bf158041d3e2
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2021-06-30 22:42:37 -0400

    Create IDAIPermit.sol
    create separate interface for dai-derived permit

---
+++ contracts/interfaces/IDAIPermit.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+pragma solidity 0.6.12;
+
+/// @title IDAIPermit
+/// @dev This interface composes DAI-derived {permit}.
+interface IDAIPermit {
+    /// @dev DAI-derived {permit}.
+    function daiPermit(
+        address holder,
+        address spender,
+        uint256 nonce,
+        uint256 expiry,
+        bool allowed,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external;
+}
