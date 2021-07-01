3d7315ec6f4f703152a37e25f09e7fa865f6a518
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2021-07-01 18:58:00 -0400

    Create IERC20.sol
    ''

---
+++ contracts/uniswapv2/2x/IERC20.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+pragma solidity 0.7.6;
+
+/// @title IERC20
+/// @notice This interface composes ERC-20 token calls with common extensions.
+interface IERC20 {
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address account) external view returns (uint256);
+    function allowance(address owner, address spender) external view returns (uint256);
+    function approve(address spender, uint256 amount) external returns (bool);
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    /// @dev EIP-2612 {permit}.
+    function permit(
+        address owner,
+        address spender,
+        uint256 value,
+        uint256 deadline,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external;
+}
