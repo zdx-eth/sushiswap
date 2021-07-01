c2e4faac53da524eb57c8066f1c5254bab658fbc
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2021-06-30 22:23:11 -0400

    Update IERC20.sol
    add dai-derived permit to interface, tweaks, use GPL ~ (consideration: abstract {permit} in SushiERC20?)

--- contracts/interfaces/IERC20.sol
+++ contracts/interfaces/IERC20.sol
@@ -1,4 +1,5 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 pragma solidity 0.6.12;
 
 interface IERC20 {
@@ -9,6 +10,26 @@ interface IERC20 {
     event Transfer(address indexed from, address indexed to, uint256 value);
     event Approval(address indexed owner, address indexed spender, uint256 value);
 
-    // EIP 2612
-    function permit(address owner, address spender, uint256 value, uint256 deadline, uint8 v, bytes32 r, bytes32 s) external;
+    /// @dev EIP-2612 {permit}.
+    function permit(
+        address owner,
+        address spender,
+        uint256 value,
+        uint256 deadline,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external;
+    
+    /// @dev DAI-derived {permit}.
+    function daiPermit(
+        address holder,
+        address spender,
+        uint256 nonce,
+        uint256 expiry,
+        bool allowed,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external;
 }
